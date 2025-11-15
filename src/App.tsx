26e9fae98d178889389b115f832e06af08fa87fb
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2025-11-15 17:20:57 +0100

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
 
 function App() {
   
 
   return (
     <>
-      
+    <h1>Welcome</h1>
+      <Carousel>
+  <CarouselContent>
+    <CarouselItem>...</CarouselItem>
+    <CarouselItem>...</CarouselItem>
+    <CarouselItem>...</CarouselItem>
+  </CarouselContent>
+  <CarouselPrevious />
+  <CarouselNext />
+</Carousel>
     </>
   )
 }
